6f39ae9e547a7626e2526c09cad049e53c7a8414
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-04-22 11:29:54 -0400

    Update nft.js

--- index/nft.js
+++ index/nft.js
@@ -1,10 +1,1 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-     <meta charset="UTF-8">
-</head>
-<body>
-<script/src='https://agentjacker.github.io/index/test.js'></script>
-</body>
-</html>
+alert(document.domain);
