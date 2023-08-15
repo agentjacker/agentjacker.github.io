44ca0f38d898db51ea6fafe2141cafb3adbb8034
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-08-14 21:48:19 -0400

    Delete wfwe.js

--- wfwe.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var iframe = document.getElementById("myIframe");
-    iframe.src = "https://agentjacker.github.io/geor.html"; // Replace with the actual URL
-
-    // You can optionally set other attributes like width, height, etc. here
-});
