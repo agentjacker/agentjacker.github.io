bde3b0de09520c9e56822abb3e63ffa2993c0c83
agentjacker <114850373+agentjacker@users.noreply.github.com>
2024-04-15 19:22:48 -0400

    Update nft.js

--- index/nft.js
+++ index/nft.js
@@ -1,1 +1,16 @@
-alert(document.domain);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Alert on Visit</title>
+<script>
+  window.onload = function() {
+    alert("Welcome to the website!");
+  };
+</script>
+</head>
+<body>
+<!-- Your page content here -->
+</body>
+</html>
